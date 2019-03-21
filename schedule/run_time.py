7675566deaa5663d758398a452dae9b98436d92d
caolihong <1234>
2019-03-21 10:25:50 +0800

    modify schedule case x

--- schedule/run_time.py
+++ schedule/run_time.py
@@ -21,7 +21,7 @@ producer = MyProducer()
 
 def increment_reload_db(args):
     """
-    定时查看数据库更新信息，并发给kafka
+    定时查看数据库更新信息，并发给任务给kafka
     :param args:
     :return:
     """
@@ -33,23 +33,5 @@ def increment_reload_db(args):
     producer.send(msg_data)
 
     db_handler.close()
-
-def my_listener(event):
-    """
-    监听循环任务的EVENT_JOB_ERROR事件的处理函数，打印错误日志
-    :param event: ERROR事件
-    :return:
-    """
-    if event.exception:
-        print 'increment_reload_db Task Run ERROR ！Information: [', event.exception, ']'
-
-#scheduler = BlockingScheduler()
-# interval
-#scheduler.add_job(func=increment_reload_db, args=('循环任务',), next_run_time=datetime.datetime.now() + datetime.timedelta(seconds=float(1.0)), id='increment_reload_db_task')
-
-#scheduler.add_listener(my_listener, EVENT_JOB_ERROR)
-#scheduler._logger = logging
-
-#scheduler.start()
 
 increment_reload_db("")
